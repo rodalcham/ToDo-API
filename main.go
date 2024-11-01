60f8c758c737c16d3787453a8737d5d27beb0d3d
Bankhardt Mate <bankhardtmate@gmail.com>
2024-11-01 20:06:12 +0100

    mark1

--- main.go
+++ main.go
@@ -1,9 +1,10 @@
 package main
 
 import (
-    "encoding/json"
     "fmt"
+    "html/template"
     "net/http"
+    "strconv"
 )
 
 // Todo struct defines the fields for a to-do item.
@@ -17,51 +18,70 @@ type Todo struct {
 var todos []Todo
 var currentID = 0
 
-// Create adds a new todo to the list
-func create(title string) Todo {
-    currentID++
-    todo := Todo{ID: currentID, Title: title, Done: false}
-    todos = append(todos, todo)
-    return todo
+// Serve HTML Page
+func homePageHandler(w http.ResponseWriter, r *http.Request) {
+    tmpl, err := template.ParseFiles("./index.html")
+    if err != nil {
+        http.Error(w, "Could not load HTML page: "+err.Error(), http.StatusInternalServerError)
+        return
+    }
+    tmpl.Execute(w, todos)
 }
 
-// Read returns all todos as JSON
-func read(w http.ResponseWriter, r *http.Request) {
-    w.Header().Set("Content-Type", "application/json")
-    json.NewEncoder(w).Encode(todos)
+// Create a new todo from the form submission
+func createTodoHandler(w http.ResponseWriter, r *http.Request) {
+    if r.Method == "POST" {
+        title := r.FormValue("title")
+        currentID++
+        todo := Todo{ID: currentID, Title: title, Done: false}
+        todos = append(todos, todo)
+    }
+    http.Redirect(w, r, "/", http.StatusSeeOther)
 }
 
-// Update marks a todo as done or updates its title
-func update(id int, title string, done bool) bool {
-    for i, todo := range todos {
-        if todo.ID == id {
-            todos[i].Title = title
-            todos[i].Done = done
-            return true
+// Toggle Done status of a todo
+func toggleDoneHandler(w http.ResponseWriter, r *http.Request) {
+    idStr := r.URL.Query().Get("id")
+    id, err := strconv.Atoi(idStr)
+    if err != nil {
+        http.Error(w, "Invalid id parameter", http.StatusBadRequest)
+        return
+    }
+    for i := range todos {
+        if todos[i].ID == id {
+            todos[i].Done = !todos[i].Done
+            break
         }
     }
-    return false
+    http.Redirect(w, r, "/", http.StatusSeeOther)
 }
 
-// Delete removes a todo from the list
-func delete(id int) bool {
+// Delete a todo
+func deleteTodoHandler(w http.ResponseWriter, r *http.Request) {
+    idStr := r.URL.Query().Get("id")
+    id, err := strconv.Atoi(idStr)
+    if err != nil {
+        http.Error(w, "Invalid id parameter", http.StatusBadRequest)
+        return
+    }
     for i, todo := range todos {
         if todo.ID == id {
             todos = append(todos[:i], todos[i+1:]...)
-            return true
+            break
         }
     }
-    return false
+    http.Redirect(w, r, "/", http.StatusSeeOther)
 }
 
 func main() {
-    // Initialize the server and routes
-    http.HandleFunc("/todos", read) // Route to get all todos as JSON
+    http.HandleFunc("/", homePageHandler)         // Home page with form and todo list
+    http.HandleFunc("/todo/create", createTodoHandler)  // Create a new todo
+    http.HandleFunc("/todo/toggle", toggleDoneHandler)  // Toggle done status
+    http.HandleFunc("/todo/delete", deleteTodoHandler)  // Delete a todo
 
-    // Adding some initial todos
-    create("Learn Go")
-    create("Implement REST API")
-    
     fmt.Println("Server running at http://localhost:8080")
-    http.ListenAndServe(":8080", nil)
+    err := http.ListenAndServe(":8080", nil)
+    if err != nil {
+        fmt.Println("Error starting server:", err)
+    }
 }
